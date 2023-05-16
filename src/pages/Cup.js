a308b40216b3113a1f86aaceedb8bcdf323566d9
Imran <huseynlimran@gmail.com>
2023-05-16 13:05:26 +0400

    data

--- src/pages/Cup.js
+++ src/pages/Cup.js
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function Cup() {
   return (
-    <div className='cup'>
-        <div className="cupimg">
+    <div className="cup">
+      <div className="cupimg">
         <img src="img/cup.png" alt="" />
-        <img src="img/allwhite.png" className='whitelogo' alt="" />
-        </div>
-        <img src="img/cupbg.jpg" className='bgcup' alt="" />
+        <img src="img/cup2.png" alt="" />
+      </div>
     </div>
-  )
+  );
 }
