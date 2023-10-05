ac776690db576dc69ec3234f8971c2d780687f99
huseynlimran <you@example.com>
2023-10-05 14:33:33 +0400

    data

--- src/pages/Swiper.js
+++ src/pages/Swiper.js
@@ -1,22 +1,13 @@
 import React from "react";
-import { Pagination } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
 
 export default function Slide() {
   return (
     <>
-      <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
-        <SwiperSlide>
-          <video autoPlay loop playsInline muted className="video">
-            <source src="img/video2.mp4" />
-          </video>
-        </SwiperSlide>
-        <SwiperSlide>
-          <video autoPlay loop playsInline muted className="video">
-            <source src="img/video3.mp4" />
-          </video>
-        </SwiperSlide>
-      </Swiper>
+
+      <video autoPlay loop playsInline muted className="video">
+        <source src="img/videobg.mp4" />
+      </video>
+
     </>
   );
 }
