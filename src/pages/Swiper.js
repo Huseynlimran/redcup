182ed154b547059ee2f5dc4934fb1ed53c5762d0
huseynlimran <you@example.com>
2023-10-05 14:42:54 +0400

    datA

--- src/pages/Swiper.js
+++ src/pages/Swiper.js
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function Slide() {
   return (
-    <>
+    <div className="swipervd">
 
       <video autoPlay loop playsInline muted className="video">
         <source src="img/videobg.mp4" />
       </video>
 
-    </>
+    </div>
   );
 }
